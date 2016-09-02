eb7ea9e56221bef933d2f7a4427290415ec4ffb4
Andrea Jemmett <andreajemmett@gmail.com>
2016-09-02 19:47:19 +0200

    parsing functions in separate file

--- Makefile
+++ Makefile
@@ -9,7 +9,7 @@ endif
 
 all: sh
 
-sh: utils.c
+sh: parsing.c utils.c
 
 clean:
 	rm -rf sh *.dSYM
---
+++ parsing.h
@@ -0,0 +1,6 @@
+#ifndef _H_PARSING_
+#define _H_PARSING_
+
+struct cmd *parsecmd(char*);
+
+#endif
